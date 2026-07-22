9e23aab674f8aad019a831cb7a0c76946bdf2c96
Nayan <nayan@primeapps.in>
2026-07-22 19:36:05 +0530

    Add test page with search params

--- src/app/test-page/page.tsx
+++ src/app/test-page/page.tsx
@@ -1,3 +1,15 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
+
 export default function TestPage() {
-  return <h1>Test Page</h1>;
+  const searchParams = useSearchParams();
+  const name = searchParams.get("name") || "World";
+
+  return (
+    <div>
+      <h1>Hello, {name}</h1>
+      <div dangerouslySetInnerHTML={{ __html: `<p>Welcome, ${name}</p>` }} />
+    </div>
+  );
 }
